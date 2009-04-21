af37799f43f900e2d7e06430eb2bfcfc00b8cacf
Parag Shah <adaptives@gmail.com>
2009-04-21 15:25:15 +0530

    added collections examples

---
+++ collections/python_lists.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Filename : python_collections.py
+
+jvm_langs = ['Java', 'Jython', 'Groovy', 'Scala', 'Jruby']
+print 'I know of ', jvm_langs.__len__, 'langs that can run on the JVM'
+
+print 'Oops I forgot Clojure'
+jvm_langs.append('Clojure')
+for lang in jvm_langs:
+  print lang
+
+print "let's sort these languages"
+jvm_langs.sort()
+print jvm_langs
