cc85346c95b6744834848c91ca67e1c06a553771
Parag Shah <adaptives@gmail.com>
2009-04-21 16:57:38 +0530

    adding file to explain tuples in Python

---
+++ collections/python_tuples.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Filename : python_tuples.py
+
+#Note: Python tuples are immutable just like strings
+
+jvm_langs = ('Java', 'Jython', 'JRuby')
+print jvm_langs
+
+print 'Oops I forgot Scala and Groovy but I cannot add to an existing tuple'
+
+jvm_langs_revised = ('Scala', 'Groovy', 'Clojure', jvm_langs)
+#Notice that the earlier sequence is retained and not flattened
+print jvm_langs_revised
+
