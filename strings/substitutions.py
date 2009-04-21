25cca6192b6ff7d3f118bb921a545f3e64e30602
Parag Shah <adaptives@gmail.com>
2009-04-21 16:10:03 +0530

    Added some comments

--- strings/substitutions.py
+++ strings/substitutions.py
@@ -11,10 +11,9 @@ print "%(greeting)s %(name)s how are you?" % locals()
 
 print Template('$greeting $name how are you?').substitute(locals())
 
+#For Python 3.0
 #print("{greeting} {name} how are you?".format(**locals()))
 
-
-print("My first program; {greeting}".format(**locals()))
 
 
 
