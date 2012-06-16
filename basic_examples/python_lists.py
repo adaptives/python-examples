4b63e2bb910a946eb61837f2acff0aa5a1df15b2
Parag Shah <adaptives@gmail.com>
2012-06-16 22:02:38 +0530

    made a few changes to the python_lists.py file

--- basic_examples/python_lists.py
+++ basic_examples/python_lists.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python
-# Filename : python_collections.py
+# Filename : python_lists.py
 
 jvm_langs = ['Java', 'Jython', 'Groovy', 'Scala', 'Jruby']
-print 'I know of ', jvm_langs.__len__, 'langs that can run on the JVM'
+
+#How many JVM langs do you know ?
+print 'I know of ', jvm_langs.__len__(), 'langs that can run on the JVM'
+
+#It's not a good idea to directly us __xxx__ methods
+#A better way is. Remember there is usually a top level function which  
+#is the idoimatic way to access the __xxx__method
+print 'I know of ', len(jvm_langs), 'langs that can run on the JVM'
 
 print 'Oops I forgot Clojure'
 jvm_langs.append('Clojure')
+
+#Let's iterate across the list
 for lang in jvm_langs:
   print lang
+  
+#Can we get the 3rd element of the list ?
+print "The 3rd JVM language is ", jvm_langs[2]
+print "The first 3 JVM languages are ", jvm_langs[:3]
+print "The 2nd to 4th JVM languages are ", jvm_langs[1:4]
 
 print "let's sort these languages"
 jvm_langs.sort()
