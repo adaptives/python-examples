16edb2d4be4c16d549157ca90eec8ba16d40b90a
Parag Shah <adaptives@gmail.com>
2012-06-16 21:37:46 +0530

    made a few changes to the file using_modules.py

--- basic_examples/using_modules.py
+++ basic_examples/using_modules.py
@@ -4,8 +4,9 @@ import sys
 import my_module
 
 #printing all the command line arguments provided to this program
-print 'sys.path = ', sys.argv
+print "cmd args to this program are '", sys.argv, "'"
 
 print 'The PYTHONPATH is ', sys.path 
 
+#Let's call a function from my_module
 my_module.sayHello()
