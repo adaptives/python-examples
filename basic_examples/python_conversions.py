5627684bbe7abcd558fd1cea83b0ee1a9287d89c
Parag Shah <adaptives@gmail.com>
2012-06-16 21:34:55 +0530

    added file to explain type conversions in Python

---
+++ basic_examples/python_conversions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# Filename : python_conversions
+
+#num is of type int
+num = 8 
+#eight is of type str
+eight = str(num)
+print "eight is of type ", type(eight)
+
+#num8 is of type int 
+num8 = int(eight)
+print "num8 is of type ", type(num8)
+
+t = True
+strt = str(t)
+print "strt is of type ", type(strt)
