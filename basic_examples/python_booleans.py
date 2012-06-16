797a3d5ece183d2d6d71ae11587972aef7e562df
Parag Shah <adaptives@gmail.com>
2012-06-16 21:06:50 +0530

    added example for python booleans

---
+++ basic_examples/python_booleans.py
@@ -0,0 +1,12 @@
+print "Python has 10 type of booleans", True, " and ", False
+print "They are normally the result of comparisons"
+
+num = 8
+if num < 10:
+    print num, " is less than 10"
+else:
+    print num, " is not less than 10"
+
+
+print "What we just saw are conditional statements.
+print "It's ok if you do not understand them, we will look into them in greater details very soon."
